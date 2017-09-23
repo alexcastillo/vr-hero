b29ab8a2b201e7f9e43a6bdff5b0c46b34185ad6
Alex Castillo <alex@castillo.io>
2017-09-23 11:15:28 -0700

    feat: add support for multiple notes in the same sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
 import Soundfont from 'soundfont-player';
+
+interface Sample {
+  header: number,
+  timestamp: number,
+  status: number,
+  note: number,
+  velocity: number
+};
 
 const MIDI_SERVICE_ID = '03b80e5a-ede8-4b33-a751-6ce34ec4c700';
 const MIDI_CHARACTERISTIC = '7772e5db-3868-4112-a1a9-f2669d106bf3';
@@ -25,17 +35,63 @@ export class AppComponent {
 
   audioContext = new AudioContext();
   instrument = Soundfont.instrument(this.audioContext, MIDI_INSTRUMENT);
+  characteristic = null;
+  notes$ = null;
   playing: {[key: number]: any} = {};
+  noteFilters = [
+    // Only notes between 30 and 79 are musical notes, the rest are artifacts and 
+    // controller commands. Notes in the 80 range are created from pressing the 
+    // controller buttons. Notes in the 100 range are an artifact created by pressing 
+    // the fretboard without strumming the string.
+    ({ note }) => note >= 30 && 79 >= note
+  ];
 
-  async playNote (note: number, velocity: number) {
+  async observeNotes () {
+    this.notes$ = Observable
+      .fromEvent(this.characteristic, 'characteristicvaluechanged')
+      .mergeMap(this.bufferToSamples)
+      .filter(sample => this.filterMusicalNotes(sample))
+      .do(sample => this.onMidi(sample))
+      .subscribe(console.log);
+  }
+
+  bufferToSamples (event) {
+    let samples = [];
+    const buffer = new Uint8Array(event.target.value.buffer);
+    const [ header, ...midi ] = Array.from(buffer);
+    while (midi.length) {
+      const [ timestamp, status, note, velocity ] = midi.splice(0, 4);
+      samples.push({ header, timestamp, status, note, velocity }); 
+    }
+    return samples;
+  }
+
+  filterMusicalNotes (sample: Sample) {
+    return this.noteFilters
+      .every(filter => filter(sample))
+  }
+
+  onMidi (sample: Sample) {
+    const { status, note, velocity } = sample;
+    if (status >= 0x80 && status < 0x90) {
+      this.stopNote(sample);
+    }
+    if (status >= 0x90 && status < 0xa0) {
+      this.playNote(sample);
+    }
+  }
+
+  async playNote (sample: Sample) {
+    const { note, velocity } = sample;
     if (!velocity) {
-      return this.stopNote(note);
+      return this.stopNote(sample);
     }
     const guitar = await this.instrument;
     this.playing[note] = guitar.play(note, null, {gain: velocity / 127.0});
   }
 
-  stopNote(note: number) {
+  stopNote(sample: Sample) {
+    const { note } = sample;
     if (this.playing[note]) {
       this.playing[note].stop();
       this.playing[note] = null;
@@ -48,21 +104,8 @@ export class AppComponent {
     });
     const gatt = await device.gatt.connect();
     const service = await gatt.getPrimaryService(MIDI_SERVICE_ID);
-    const characteristic = await service.getCharacteristic(MIDI_CHARACTERISTIC);
-
-    characteristic.addEventListener('characteristicvaluechanged', () => {
-      const buffer = new Uint8Array(characteristic.value.buffer);
-      const [ header, timestamp, status, d1, d2 ] = Array.from(buffer);
-      console.log('new data', buffer);
-      if (status >= 0x80 && status < 0x90) {
-        this.stopNote(d1);
-      }
-      if (status >= 0x90 && status < 0xa0) {
-        this.playNote(d1, d2);
-      }
-    });
-
-    characteristic.startNotifications();
-    console.log('ready!');
+    this.characteristic = await service.getCharacteristic(MIDI_CHARACTERISTIC);
+    this.characteristic.startNotifications();
+    this.observeNotes();
   }
 }
