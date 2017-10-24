23ef038994cde5e3cdf6dd5abb39518b711e287a
Alex Castillo <alex@castillo.io>
2017-10-23 23:09:52 -0700

    feat: fret number + song notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,19 @@ export class AppComponent {
     this.onConnect();
   }
 
+  firstFrets = [52, 47, 43, 38, 33 , 28];
+
+  // @TODO: Need to add startTime in milliseconds from the song start time
+  demoSong = [
+    { header: 189, timestamp: 228, status: 147, note: 40, velocity: 105 },
+    { header: 135, timestamp: 147, status: 147, note: 40, velocity: 81 },
+    { header: 140, timestamp: 193, status: 147, note: 43, velocity: 87 },
+    { header: 146, timestamp: 179, status: 147, note: 40, velocity: 83 },
+    { header: 155, timestamp: 241, status: 147, note: 38, velocity: 99 },
+    { header: 163, timestamp: 245, status: 148, note: 36, velocity: 70 },
+    { header: 183, timestamp: 129, status: 148, note: 35, velocity: 102 }
+  ];
+
   onConnect () {
     this.jamstik.midi
       .subscribe(sample => {
@@ -32,14 +45,18 @@ export class AppComponent {
 
   onMidi (sample) {
     const { status, note, velocity } = sample;
+    const stringId = status - 0x90;
+    const fret = note - this.firstFrets[stringId];
+
     if (status >= 0x80 && status < 0x90) {
       this.stopNote(sample);
     }
     if (status >= 0x90 && status < 0xa0) {
       this.playNote(sample);
       this.realtime.addEvent({
-        stringId: 1,
-        note: 50
+        note,
+        fret,
+        stringId
       });
       console.log(sample);
     }
